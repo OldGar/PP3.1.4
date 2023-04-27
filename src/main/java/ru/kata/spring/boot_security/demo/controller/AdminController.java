6f98b91dad8fccb9f9ac0563d5d44c7681946f88
Igor Golubev <prostogar@gmail.com>
2023-04-27 13:32:54 +0300

    First commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,70 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.molel.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final RoleService roleService;
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/admin")
+    public String getAdminPage(Principal principal, Model model) {
+        model.addAttribute("newUser", new User());
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        //model.addAttribute("updateUser", new User());
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "admin/admin";
+    }
+
+    @GetMapping("/user/page")
+    public String getAddUserPage(Model model) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("user", new User());
+        return "admin/save";
+    }
+
+    @PostMapping("/user")
+    public String addUser(@ModelAttribute("newUser") User user) {
+        userService.save(user);
+        return "redirect:/admin/admin";
+    }
+
+    @GetMapping("/user/{id}")
+    public String getUserEditPage(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        User user = userService.getById(id);
+        model.addAttribute("user", user);
+        return "admin/edit";
+    }
+
+    @PutMapping("/user/{id}")
+    public String editUser(@PathVariable(value = "id", required = false) Long id,@ModelAttribute("user") User user) {
+        userService.update(user, id);
+        return "redirect:/admin/admin";
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/admin/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/molel/Role.java
@@ -0,0 +1,70 @@
+package ru.kata.spring.boot_security.demo.molel;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
